cd7b85df54f2eff126ced24900ee6831d4c6188c
Diana Morena <aneroma@yahoo.com>
2022-08-19 20:55:11 +0100

    Movie Details page elements

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -1,48 +1,26 @@
 import React from "react";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import HomeIcon from "@material-ui/icons/Home";
-import { withRouter } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "space-around",
-    flexWrap: "wrap",
-    padding: theme.spacing(1.5),
-    margin: 0,
-  },
-  tagLine: {
-    fontSize: "1.5rem",
-  },
-}));
-
-const MovieHeader = ( { movie, history}) => {
-  const classes = useStyles();
-
+import "./headerMovie.css";
+import "../../globals/fontawesome";
+import {Icon,Grid,Header} from 'semantic-ui-react'
+const MovieHeader = ({ movie }) => {
   return (
-    <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back" onClick={() => history.goBack()} >
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
-
-      <Typography variant="h4" component="h3">
-        {movie.title}
-        <a href={movie.homepage}>
-          <HomeIcon color="primary" />
-        </a>
-        <br />
-        <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
-      </Typography>
-      <IconButton aria-label="go forward" onClick={() => history.goForward() } >
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
+  
+    <Grid>
+    <Grid.Row>
+    <Grid.Column width={3}>
+    </Grid.Column>
+    <Grid.Column width={4}>
+      <Header as="h2" id="heading">
+    {movie.title}
+          {"  "}
+          <a href={movie.homepage} >
+          <Icon name='globe'/>
+          </a>
+    </Header>
+    </Grid.Column>
+    </Grid.Row>
+    </Grid>
   );
 };
 
-export default withRouter(MovieHeader);
+export default MovieHeader;
--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,86 +1,111 @@
-import React, { useState} from "react";
-import Chip from "@material-ui/core/Chip";
-import Paper from "@material-ui/core/Paper";
-import AccessTimeIcon from "@material-ui/icons/AccessTime";
-import MonetizationIcon from "@material-ui/icons/MonetizationOn";
-import StarRate from "@material-ui/icons/StarRate";
-import NavigationIcon from "@material-ui/icons/Navigation";
-import Fab from "@material-ui/core/Fab";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import MovieReviews from "../movieReviews"
+import React, { useEffect, useState } from "react";
+import "./movieDetails.css";
+import { getRecommendations } from "../../api/tmdb-api";
+import Movie from "../recommendedCard/";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap",
-    listStyle: "none",
-    padding: theme.spacing(1.5),
-    margin: 0,
-  },
-  chip: {
-    margin: theme.spacing(0.5),
-  },
-  fab: {
-    position: "fixed",
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
 
-const MovieDetails = ({ movie }) => {  // Don't miss this!
-    const classes = useStyles();
-    const [drawerOpen, setDrawerOpen] = useState(false);
-  
-    return (
+import {Card,Flag,Container,Segment,Grid,Divider,Header,Icon} from 'semantic-ui-react'
+
+export default ({ movie}) => {
+  const [recommendeds, setRecommended] = useState([]);
+
+
+  useEffect(() => {
+    getRecommendations(movie.id).then(recommendeds => {
+      setRecommended(recommendeds);
+    });
+  }, []);
+
+
+  function sleep(ms){
+    while(ms < 100){
+      ms++;
+    }
+    reloadMovieCards();
+  }
+  const movieCards = recommendeds.map(m => (
+    <Movie key={m.id} movie={m}/>
+  ));
+
+  function reloadMovieCards(){
+    getRecommendations(movie.id).then(recommendeds => {
+      setRecommended(recommendeds);
+    });
+  }
+
+  return (
+    
     <>
-      <Typography variant="h5" component="h3">
-        Overview
-      </Typography>
+      
+    <Segment id="movieDetails">
+    <Grid columns={2} relaxed='very'>
+      <Grid.Column>
+      <Container>
+      <Header as='h2'><span>Overview</span></Header>
+      <p>{movie.overview}</p>
+      </Container>
+      </Grid.Column>
+      <Grid.Column>
+      <table class="table table-dark">
+          <tbody>
+            <tr>
+              <th>Runtime <Icon name="time"></Icon></th>
+              <td>{movie.runtime} minutes</td>
+            </tr>
+            <tr>
+              <th>Release Date <Icon name="calendar alternate outline"></Icon></th>
+              <td>{movie.release_date}</td>
+            </tr>
+            <tr>
+              <th>Genres <Icon name="film"></Icon></th>
+              <td><ul>
+              {movie.genres.map(g => (
+                <li key={g.name}>
+                  {g.name}
+                </li>
+              ))}
+              </ul></td>
+            </tr>
+            <tr>
+              <th>Spoken Languages <Icon name="assistive listening systems"></Icon></th>
+              <td><ul>
+              {movie.spoken_languages.map(lang => (
+                <li key={lang.name}>
+                  {lang.name}
+                </li>
+              ))}
+            </ul></td></tr>
+            <tr>
+              <th>Production Companies <Icon name="factory"></Icon></th>
+              <td><ul>
+              {movie.production_companies.map(pdc => (
+                <li key={pdc.name}>
+                  {pdc.name}
+                </li>
+              ))}
+              </ul></td>
+            </tr>
+            <tr>
+              <th>Production Countries <Icon name="flag"></Icon></th>
+              <td><ul>
+              {movie.production_countries.map(pc => (
+                <li key={pc.name}>
+                  <Flag name={pc.iso_3166_1.toLowerCase()} />{pc.name}
+                </li>
+              ))}
+              </ul></td>
+            </tr>
+          </tbody>
+        </table>
+      </Grid.Column>
+    </Grid>
 
-      <Typography variant="h6" component="p">
-        {movie.overview}
-      </Typography>
+    <Divider vertical><span>Details</span></Divider>
+    </Segment>
+    <Divider horizontal><span><h2>Similar Movies</h2></span></Divider>
+    {<Card.Group onClick={sleep(100)}>{movieCards}</Card.Group>}
 
-      <Paper component="ul" className={classes.root}>
-        <li>
-          <Chip label="Genres" className={classes.chip} color="primary" />
-        </li>
-        {movie.genres.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} className={classes.chip} />
-          </li>
-        ))}
-      </Paper>
-      <Paper component="ul" className={classes.root}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count}`}
-        />
-        <Chip label={`Released: ${movie.release_date}`} />
-      </Paper>
-
-      <Fab
-        color="secondary"
-        variant="extended"
-        onClick={() =>setDrawerOpen(true)}
-        className={classes.fab}
-      >
-        <NavigationIcon />
-        Reviews
-      </Fab>
-      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
-        <MovieReviews movie={movie} />
-      </Drawer>
     </>
-  );
-};
+  )};
 
-export default  MovieDetails ;
+
