bfd099202ba8115894f3320ed6bf60ce4bc529ff
Diana Morena <aneroma@yahoo.com>
2022-08-19 22:42:39 +0100

    Full review page

--- src/components/movieReview/index.js
+++ src/components/movieReview/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MovieReview =  ({ review }) => {
+export default ({ review }) => {
   return (
     <>
       <p>Review By: {review.author} </p>
@@ -8,4 +8,3 @@ const MovieReview =  ({ review }) => {
     </>
   );
 };
-export default MovieReview
